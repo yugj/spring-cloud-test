9662ac9c6474fa0e88699146500b2a27c113b125
yugj <yugj@hotmail.com>
2018-09-03 10:56:52 +0800

    chainofresponsibility

---
+++ base-test/src/main/java/indi/yugj/test/springclound/basetest/designpattern/chainofresponsibility/MainTest.java
@@ -0,0 +1,12 @@
+package indi.yugj.test.springclound.basetest.designpattern.chainofresponsibility;
+
+/**
+ * @author yugj
+ * @date 18/9/3 10:55.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+    }
+}
