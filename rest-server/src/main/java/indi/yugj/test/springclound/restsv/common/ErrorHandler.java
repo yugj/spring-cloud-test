3c3d543aa5b5da8e47f59ae3094063869405cd05
yugj <yugj@hotmail.com>
2018-09-05 23:06:18 +0800

    actuator

---
+++ rest-server/src/main/java/indi/yugj/test/springclound/restsv/common/ErrorHandler.java
@@ -0,0 +1,64 @@
+package indi.yugj.test.springclound.restsv.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 内部异常处理
+ * @author yugj
+ * @date 18/8/29 15:08.
+ */
+@RestController
+public class ErrorHandler extends AbstractErrorController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorHandler.class);
+
+    @Value("${error.path:/error}")
+    private String errorPath;
+
+    public ErrorHandler(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    @RequestMapping(value = "${error.path:/error}", produces = "application/json;charset=UTF-8")
+    public ResponseEntity error(HttpServletRequest request) {
+
+        HttpStatus httpStatus = getStatus(request);
+        int code = httpStatus.value();
+
+        LOGGER.warn("sys error,http status:" + code);
+
+        if (HttpStatus.NOT_FOUND.value() == code) {
+            return new ResponseEntity<>(new CgiResp("404", "404"), HttpStatus.NOT_FOUND);
+        }
+
+        if (HttpStatus.UNAUTHORIZED.value() == code) {
+            //返回401有助于浏览器识别弹出鉴权窗口 比如chrome浏览器,不然需要请求带鉴权数据,比较麻烦
+            return new ResponseEntity<>(new CgiResp("401", "401"), HttpStatus.UNAUTHORIZED);
+        }
+
+        if (HttpStatus.METHOD_NOT_ALLOWED.value() == code) {
+            return new ResponseEntity<>(new CgiResp("405", "405"), HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        if (HttpStatus.BAD_REQUEST.value() == code) {
+            return new ResponseEntity<>(new CgiResp("400", "400"), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(new CgiResp("500", "500"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return errorPath;
+    }
+}
