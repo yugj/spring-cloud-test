fcc2ff959b4de3e377f94257897a3ddd6a207cf4
yugj <yugj@hotmail.com>
2018-09-10 10:28:10 +0800

    config test

---
+++ config-client/src/main/java/indi/yugj/test/springclound/configclient/TestController.java
@@ -0,0 +1,27 @@
+package indi.yugj.test.springclound.configclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author yugj
+ * @date 18/9/10 09:48.
+ */
+@Controller
+@RequestMapping("/config-client")
+public class TestController {
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hell1")
+    @ResponseBody
+    public String hell1() {
+
+        String test = env.getProperty("test.is.checkRules.test.req");
+        return "hell-1";
+    }
+}
