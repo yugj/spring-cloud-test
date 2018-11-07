430825d53431b3ec73a0841e2b6ab4951af8f114
yugj <yugj@hotmail.com>
2018-11-07 23:53:17 +0800

    comments

--- eureka-client/src/main/java/indi/yugj/test/springclound/eurekaclient/EurekaClientInfoController.java
+++ eureka-client/src/main/java/indi/yugj/test/springclound/eurekaclient/EurekaClientInfoController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
+ * DiscoveryClient refreshRegistry 默认包访问权限
+ * 意味着刷新配置不暴露
+ * 可能由于某些原因需要保护现有轮询机制?
+ * https://github.com/spring-cloud/spring-cloud-netflix/issues/1682#issuecomment-278994223
+ * There is no way to force a client to poll the Eureka server. What @Aloren is suggesting is that you adjust the interval at which the client fetches the registry information from the server so it happens quicker/more often.
  * @author yugj
  * @date 18/11/7 22:06.
  */
