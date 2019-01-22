95a7a872899e1533af6b9552c5994ddf888b55d7
yugj <yugj@hotmail.com>
2019-01-22 16:50:21 +0800

    stream demo

---
+++ stream/src/main/java/indi/yugj/test/springclound/stream/StreamStart.java
@@ -0,0 +1,12 @@
+package indi.yugj.test.springclound.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamStart.class, args);
+    }
+}
