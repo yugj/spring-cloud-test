65ad89dbd8119eca75b0ae6d105028c83c12492f
yugj <yugj@hotmail.com>
2019-01-22 17:49:53 +0800

    stream demo

---
+++ stream/src/main/java/indi/yugj/test/springclound/stream/StreamReceiver.java
@@ -0,0 +1,24 @@
+package indi.yugj.test.springclound.stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yugj
+ * @date 2019/1/22 下午4:54.
+ */
+@Service
+public class StreamReceiver {
+
+    private static final Logger log = LoggerFactory.getLogger(StreamReceiver.class);
+
+    @StreamListener(Processor.INPUT)
+    public void process(String message) {
+
+        log.info("receiver ------------>>" + message);
+
+    }
+}
---
+++ stream/src/test/java/indi/yugj/test/springclound/strem/RabbitMqClientTest.java
@@ -0,0 +1,43 @@
+package indi.yugj.test.springclound.strem;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+/**
+ * @author yugj
+ * @date 2019/1/14 下午5:12.
+ */
+public class RabbitMqClientTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMqClientTest.class);
+
+    public static void main(String[] args) {
+
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+
+        connectionFactory.setHost("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+
+        JSONObject message = new JSONObject();
+        message.put("userId", 888);
+
+        String queueName = "q.hell.stream";
+
+        LOGGER.info("[rabbit mq send] queue:{},message:{}", queueName, message.toJSONString());
+
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.convertAndSend(queueName, message.toJSONString());
+
+        System.exit(0);
+
+    }
+
+}
