52786ad47d46c4f5a44b7096fd998ab365322a03
yugj <yugj@hotmail.com>
2019-08-05 11:44:57 +0800

    rate limit handler error

--- zuul/src/main/java/indi/yugj/test/springcloud/zuul/ratelimit/CustomRateLimitConfiguration.java
+++ zuul/src/main/java/indi/yugj/test/springcloud/zuul/ratelimit/CustomRateLimitConfiguration.java
@@ -3,6 +3,8 @@ package indi.yugj.test.springcloud.zuul.ratelimit;
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitKeyGenerator;
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitUtils;
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitProperties;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.DefaultRateLimiterErrorHandler;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.RateLimiterErrorHandler;
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.support.DefaultRateLimitKeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.Route;
@@ -23,6 +25,27 @@ public class CustomRateLimitConfiguration {
 
     @Autowired
     private CustomRateLimitProperties customRateLimitProperties;
+
+    @Bean
+    public RateLimiterErrorHandler rateLimitErrorHandler() {
+
+        return new DefaultRateLimiterErrorHandler() {
+            @Override
+            public void handleSaveError(String key, Exception e) {
+                System.out.println();
+            }
+
+            @Override
+            public void handleFetchError(String key, Exception e) {
+                // custom code
+            }
+
+            @Override
+            public void handleError(String msg, Exception e) {
+                // custom code
+            }
+        };
+    }
 
     @Bean
     public RateLimitKeyGenerator ratelimitKeyGenerator(RateLimitProperties properties, RateLimitUtils rateLimitUtils) {
