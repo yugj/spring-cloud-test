4ebbc0c660bfdc06a1f6290e8cdbf07adc1458e6
yugj <yugj@hotmail.com>
2020-03-17 14:20:34 +0800

    sleuth

--- consul-grpc-client/src/main/java/cn/yugj/test/consulgrpcclient/TestController.java
+++ consul-grpc-client/src/main/java/cn/yugj/test/consulgrpcclient/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 /**
  * @author yugj
@@ -16,8 +17,13 @@ public class TestController {
     @Autowired
     private GrpcClientService client;
 
+    private static final Logger logger = Logger.getLogger(TestController.class.getName());
+
     @GetMapping("/hell")
     public String hell() throws ExecutionException, InterruptedException {
+
+        logger.info("hell client");
+
         client.sayHello();
 
         return "ok";
--- consul-grpc-client2/src/main/java/consulgrpcclient/TestController.java
+++ consul-grpc-client2/src/main/java/consulgrpcclient/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 /**
  * @author yugj
  * @date 2020/2/26 16:51.
@@ -14,8 +16,14 @@ public class TestController {
     @Autowired
     private GrpcClientService2 client;
 
+    private static final Logger logger = Logger.getLogger(TestController.class.getName());
+
+
     @GetMapping("/hell")
     public String hell() {
+
+        logger.info("hello client");
+
         client.sayHell();
 
         return "ok";
