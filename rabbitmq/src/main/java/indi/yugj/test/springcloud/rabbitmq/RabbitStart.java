509a23fa585a69f51c4c9bae20b2062c6f0f8cf8
yugj <yugj@hotmail.com>
2020-07-08 16:29:17 +0800

    rabbit mq json msg

--- rabbitmq/src/main/java/indi/yugj/test/springcloud/rabbitmq/RabbitStart.java
+++ rabbitmq/src/main/java/indi/yugj/test/springcloud/rabbitmq/RabbitStart.java
@@ -1,7 +1,10 @@
 package indi.yugj.test.springcloud.rabbitmq;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Description:
@@ -12,5 +15,14 @@ public class RabbitStart {
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitStart.class, args);
+    }
+
+    /**
+     * 配置json转换 spring体系处理mq json 需要
+     * @return MessageConverter
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
